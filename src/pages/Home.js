4c052491016ef6a734ef78b74bc3494bd3c20451
heisenberg <sasalman00@gmail.com>
2023-03-24 18:15:52 +0500

    horizontal scrollbar done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
 import Exercises from '../components/Exercises';
 import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
 
-const home = () => {
+const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
     <Box>
-      <HeroBanner /> 
-      <SearchExercises />
-      <Exercises />
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
     </Box>
   )
 }
 
-export default home
+export default Home
