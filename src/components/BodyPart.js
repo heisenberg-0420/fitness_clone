b486a9ffda031f0a96d0ace3180ea8f26fb24716
heisenberg <sasalman00@gmail.com>
2023-03-26 23:41:01 +0500

    home page done

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -9,6 +9,10 @@ const BodyPart = ({item, setBodyPart, bodyPart}) => {
         borderTop: bodyPart === item ? '4px solid #ff2625' : '', 
         backgroundColor: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '280px', cursor: 'pointer', gap: '47px'
       }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({top: 1800, left: 100, behavior: 'smooth'});
+      }}
     >
       <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px'}} />
       <Typography fontSize='24px' fontWeight='bold' color='#3a1212' textTransform='capitalize'>
